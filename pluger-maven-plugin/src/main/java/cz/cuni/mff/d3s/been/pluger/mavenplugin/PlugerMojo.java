38d268d56390f98f190d2a12e0482ecddb594e57
tpalusga <tpalusga@vendavo.com>
2015-02-16 00:13:11 +0100

    pluger plugin is now able to resolve project dependencies using aether library - also fixed plugin's pom.xml to define usage of maven-plugin-plugin and also removed unnecessary dependencies and properties from parent pom.xml

--- pluger-maven-plugin/src/main/java/cz/cuni/mff/d3s/been/pluger/mavenplugin/PlugerMojo.java
+++ pluger-maven-plugin/src/main/java/cz/cuni/mff/d3s/been/pluger/mavenplugin/PlugerMojo.java
@@ -3,14 +3,69 @@ package cz.cuni.mff.d3s.been.pluger.mavenplugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.collection.CollectRequest;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.graph.DependencyFilter;
+import org.eclipse.aether.graph.DependencyNode;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.resolution.DependencyRequest;
+import org.eclipse.aether.resolution.DependencyResolutionException;
+import org.eclipse.aether.util.artifact.JavaScopes;
 
-@Mojo(name = "pluginate")
+import java.util.Collection;
+import java.util.List;
+
+@Mojo(name = "pluginate", threadSafe = true)
 public class PlugerMojo extends AbstractMojo {
 
-    @Override
+    @Component
+    private RepositorySystem repoSystem;
+
+    @Parameter(defaultValue = "${repositorySystemSession}", readonly = true)
+    private RepositorySystemSession repoSession;
+
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject mavenProject;
+
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info( "Hello, world." );
+
+        String artifactCoords = String.format("%s:%s:%s",
+                mavenProject.getGroupId(),
+                mavenProject.getArtifactId(),
+                mavenProject.getVersion());
+
+        try {
+            Artifact artifact = new DefaultArtifact(artifactCoords);
+            Collection<ArtifactResult> dependencies = collectDependencies(artifact);
+            for (ArtifactResult dependency : dependencies) {
+                System.out.println(dependency.toString());
+            }
+        } catch (IllegalArgumentException | DependencyResolutionException e) {
+            throw new MojoFailureException(e.getMessage(), e);
+        }
     }
+
+    public Collection<ArtifactResult> collectDependencies(Artifact artifact) throws DependencyResolutionException {
+        Dependency dependency = new Dependency(artifact, JavaScopes.RUNTIME);
+        CollectRequest collectRequest = new CollectRequest(dependency, null);
+
+        DependencyRequest dependencyRequest = new DependencyRequest(collectRequest, new DependencyFilter() {
+            @Override
+            public boolean accept(DependencyNode node, List<DependencyNode> parents) {
+                return !node.getDependency().getScope().equals(JavaScopes.TEST) && !node.getDependency().isOptional();
+            }
+        });
+        return repoSystem.resolveDependencies(repoSession, dependencyRequest).getArtifactResults();
+    }
+
 
 }
